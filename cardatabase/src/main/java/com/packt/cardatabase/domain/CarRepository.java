a350424915454ee1a848fc207401dfeb92ec2760
강화원 <corn1128@naver.com>
2023-07-24 15:14:51 +0900

    car-backend

---
+++ cardatabase/src/main/java/com/packt/cardatabase/domain/CarRepository.java
@@ -0,0 +1,16 @@
+package com.packt.cardatabase.domain;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface CarRepository extends CrudRepository<Car, Long> {
+	// 브랜드로 자동차를 검색
+	List<Car> findByBrand(@Param("brand") String brand);
+	
+	// 색상으로 검색
+	List<Car> findByColor(@Param("color") String color);
+}
